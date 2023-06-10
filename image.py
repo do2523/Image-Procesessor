6110ba7285029501806a72bc84dfbfe6479e4872
Daniel Ocampo <121493027+do262431@users.noreply.github.com>
2023-06-10 14:30:28 -0400

    Test Files

--- image.py
+++ image.py
@@ -1,5 +1,21 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 
 img = Image.open('./Pokedex/pikachu.jpg')
 
-print(img)
+filtered_img = img.filter(ImageFilter.EMBOSS) # You can look at the documentation for more types
+filtered_img.save("emboss.png", "png")  
+
+
+
+# filtered_img = img.convert("L")
+# filtered_img.save("blacknwhite.png", "png")
+
+# Creating new images from original
+# img.filter(ImageFilter.SHARPEN) # You can look at the documentation for more types
+# filtered_img = filtered_img.save("sharpen.png", "png")  
+
+# A few Properties
+# print(img.format)
+# print(img.size)
+# print(img.mode)
+
