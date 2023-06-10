da3654038ba6f57e7f0a72ee3aa28e03ae89c82c
Daniel Ocampo <121493027+do262431@users.noreply.github.com>
2023-06-10 15:18:32 -0400

    Resize Images the correct way

---
+++ JPG-to-PNG-Converter.py
@@ -0,0 +1,12 @@
+import sys
+import os  # alternative would be the pathlib module
+from PIL import Image
+
+sys.argv
+# Grab first and second argument
+
+# Check if new\ exists if not then create it
+
+# Loop through Pokedex and convert images to png
+
+# Save to the new folder
--- image.py
+++ image.py
@@ -1,21 +1,6 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('./Pokedex/pikachu.jpg')
-
-filtered_img = img.filter(ImageFilter.EMBOSS) # You can look at the documentation for more types
-filtered_img.save("emboss.png", "png")  
-
-
-
-# filtered_img = img.convert("L")
-# filtered_img.save("blacknwhite.png", "png")
-
-# Creating new images from original
-# img.filter(ImageFilter.SHARPEN) # You can look at the documentation for more types
-# filtered_img = filtered_img.save("sharpen.png", "png")  
-
-# A few Properties
-# print(img.format)
-# print(img.size)
-# print(img.mode)
-
+img = Image.open('./astro.jpg') # current size (5611, 5339)
+img.thumbnail((400, 400)) # Match aspect ratio so picture still looks good with thumbnail
+img.save("thumbnail.jpg")
+print(img.size) # (400,381)
