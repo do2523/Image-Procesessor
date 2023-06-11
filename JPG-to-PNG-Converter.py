66c8deb537502665a606ed0d95c2aa629caadaf9
Daniel Ocampo <121493027+do262431@users.noreply.github.com>
2023-06-11 08:36:50 -0400

    Add Multiple File Conversion  Capability

--- JPG-to-PNG-Converter.py
+++ JPG-to-PNG-Converter.py
@@ -2,11 +2,24 @@ import sys
 import os  # alternative would be the pathlib module
 from PIL import Image
 
-sys.argv
 # Grab first and second argument
+dir1 = sys.argv[1]
+dir2 = sys.argv[2]
 
 # Check if new\ exists if not then create it
+if not os.path.exists(dir2):
+    os.makedirs(dir2)
 
 # Loop through Pokedex and convert images to png
+for filename in os.listdir(dir1):
+    img = Image.open(f'{dir1}{filename}')
+    tempTuple = os.path.splitext(filename)
+    img.save(f'{dir2}{tempTuple[0]}.png', 'png')
+    print("Done")
 
 # Save to the new folder
+# try:
+#     if os.path.exists("./info.txt"):
+#         print("True")
+# except:
+#     print("false")
--- image.py
+++ image.py
@@ -1,6 +1,6 @@
-from PIL import Image, ImageFilter
+# from PIL import Image, ImageFilter
 
-img = Image.open('./astro.jpg') # current size (5611, 5339)
-img.thumbnail((400, 400)) # Match aspect ratio so picture still looks good with thumbnail
-img.save("thumbnail.jpg")
-print(img.size) # (400,381)
+# img = Image.open('./astro.jpg') # current size (5611, 5339)
+# img.thumbnail((400, 400)) # Match aspect ratio so picture still looks good with thumbnail
+# img.save("thumbnail.jpg")
+# print(img.size) # (400,381)
